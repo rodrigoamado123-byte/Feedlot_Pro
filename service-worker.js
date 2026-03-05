7acb6b4fc841853254f03ea970f356b84e4dcf16
rodrigoamado123-byte <rodrigoamado123@gmail.com>
2026-03-05 16:23:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,34 @@
-const CACHE_NAME = "feedlot-pro-v1";
+const CACHE_NAME = "feedlot-pro-cache-v1";
 
 const urlsToCache = [
-  "/Feedlot_Pro/",
-  "/Feedlot_Pro/index.html",
-  "/Feedlot_Pro/manifest.json",
-  "/Feedlot_Pro/icon.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
 ];
 
-self.addEventListener("install", function(event) {
+// INSTALAR SERVICE WORKER
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Cache abierto");
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", function(event) {
+// ACTIVAR SERVICE WORKER
+self.addEventListener("activate", event => {
+  console.log("Service Worker activado");
+});
+
+// INTERCEPTAR PETICIONES
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
