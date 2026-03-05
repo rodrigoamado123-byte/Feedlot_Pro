146c93a9f52fcdd422eb6c3af3f6c93cb8a2ba56
rodrigoamado123-byte <rodrigoamado123@gmail.com>
2026-03-05 17:02:48 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = 'feedlot-pro-cache-v1';
+const assets = [
+  './feedlot_pro.html',
+  './vaca_angus.png',
+  './icono.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
